69d12ea1a17cc053d97cf4e75f9321dd7fffb1ad
Brayden Winterton <bwinterton@gmail.com>
2016-07-14 18:25:43 -0600

    Added first test script and results

---
+++ test_1.sh
@@ -0,0 +1,48 @@
+
+
+
+if [ $# -le 0 ]; then
+    echo "Error: No command was specified for the test"
+    exit
+fi
+
+
+COMMAND="$1"
+HOST="http://127.0.0.1:8000"
+CONCURRENCIES=(1 10 100 1000)
+REQUESTLOADS=(100 1000 10000)
+
+echo "//////////////////////////////////////////"
+echo "/------------ Beginning Test ------------/"
+echo "//////////////////////////////////////////"
+echo "Testing command: $COMMAND"
+echo "Testing host: $HOST"
+echo
+echo "-------- Testing Status Endpoint ---------"
+echo
+
+for c in "${CONCURRENCIES[@]}"
+do
+    echo "    ---Concurrency Level $c---"
+    for n in "${REQUESTLOADS[@]}"
+    do
+	# If the concurrency is greater than the total then skip
+	if [ $c -gt $n ]; then
+	    continue
+	fi
+
+	echo "            - $n Requests -              "
+	/usr/bin/time -l $COMMAND &> /tmp/results &
+	TIMEPID=$!
+	sleep 1
+	COMMANDPID=$(pgrep -P $TIMEPID)
+	ab -c $c -n $n -r -q $HOST/status | grep -e 'across all' -e 'Complete' -e 'Failed'
+	kill $COMMANDPID
+	sleep 1
+	MEM=$(cat /tmp/results | grep maximum | awk '{print $1}')
+	echo "Max Memory Used: $(expr $MEM / 1048576)"
+	sleep 1
+    done
+done
+
+rm /tmp/results
