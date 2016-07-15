3371401a477e07a337245c5505aefa2aedf1ab67
Brayden Winterton <bwinterton@gmail.com>
2016-07-14 18:25:21 -0600

    Updated Java example to use port 8000 and logging

--- java/src/main/java/SparkTest.java
+++ java/src/main/java/SparkTest.java
@@ -1,10 +1,16 @@
 import static spark.Spark.*;
 
 public class SparkTest {
+
     public static void main(String[] args) {
+
+	port(8000); // To make consistent with Go example
+
 	get("/status", (req, res) -> "{\"status\":\"ok\"}");
 	get("/echo/:phrase", (req, res) -> {
 		return "{\"phrase\":\"" + req.params("phrase") + "\"}";
 	});
+
     }
+
 }
