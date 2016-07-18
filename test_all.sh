a2388d856f5842a4c9c6405881814b85a76ecc04
Brayden Winterton <bwinterton@gmail.com>
2016-07-18 15:14:33 -0600

    Modify test_all to fix piping oversight

--- test_all.sh
+++ test_all.sh
@@ -7,17 +7,17 @@ echo
 echo
 echo "************* Test 1 *************"
 echo "---------- Testing Spark ---------"
-./test_1.sh "java -jar java/target/spark_test-1.0-SNAPSHOT-jar-with-dependencies.jar" > test_1_spark_results.txt
+./test_1.sh "java -jar java/target/spark_test-1.0-SNAPSHOT-jar-with-dependencies.jar" &> test_1_spark_results.txt
 echo
 echo "---------- Testing Go ---------"
 echo
-./test_1.sh "./go/go_test" > test_1_go_results.txt
+./test_1.sh "./go/go_test" &> test_1_go_results.txt
 
 echo "************* Test 2 *************"
 echo
 echo "---------- Testing Spark ---------"
-./test_2.sh "java -jar java/target/spark_test-1.0-SNAPSHOT-jar-with-dependencies.jar" > test_2_spark_results.txt
+./test_2.sh "java -jar java/target/spark_test-1.0-SNAPSHOT-jar-with-dependencies.jar" &> test_2_spark_results.txt
 echo
 echo "---------- Testing Go ---------"
 echo
-./test_2.sh "./go/go_test" > test_2_go_results.txt
+./test_2.sh "./go/go_test" &> test_2_go_results.txt
